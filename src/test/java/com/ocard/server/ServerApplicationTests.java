f178c78af4c38d397afcd0cbbd47704588bc86cf
FangChenYi <fx1.github@gmail.com>
2024-01-05 01:44:58 +0800

    first commit

---
+++ src/test/java/com/ocard/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocard.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
