ca4b93f97434029e004a3187ad892a3ca7def2bc
FangChenYi <fx1.github@gmail.com>
2024-01-10 16:02:30 +0800

    feat: 新增 comment sql setup

---
+++ sql/comment/commentSetup.sql
@@ -0,0 +1,12 @@
+CREATE TABLE comment
+(
+    comment_id      INT AUTO_INCREMENT PRIMARY KEY,
+    comment_content TEXT     NOT NULL,
+    date_posted     DATETIME NOT NULL,
+    date_updated    DATETIME,
+    article_id      INT      NOT NULL,
+    FOREIGN KEY (article_id) REFERENCES article (article_id)
+);
+
+INSERT INTO comment (comment_content, date_posted, article_id)
+VALUES ('這是第一則評論', '2023-01-01 10:00:00', 1)
